e7266887a36e8c09c3f496800565605b394a2370
James Wright <james.wright@matillion.com>
2022-12-01 19:24:38 +0000

    Formats changes

--- 2021practice/day12/path.ts
+++ 2021practice/day12/path.ts
@@ -1,39 +1,41 @@
 export const getNumberOfPermutationsWithUniqueSmallValues = (input: string) => {
-    const vertices = input.split("\n");
-    const paths = [];
+  const vertices = input.split("\n");
+  const paths = [];
 
-    const traverse = (node: string, path: string[]) => {
-        // We need to shallowly copy the path
-        // rather than mutate it, otherwise
-        // we'll erroneously append sibling
-        // visitees from the above level to
-        // a completed path when endNode is encountered
-        const p = [...path, node];
+  const traverse = (node: string, path: string[]) => {
+    // We need to shallowly copy the path
+    // rather than mutate it, otherwise
+    // we'll erroneously append sibling
+    // visitees from the above level to
+    // a completed path when endNode is encountered
+    const p = [...path, node];
 
-        if (node === 'end') {
-            paths.push(p);
-            return;
-        }
+    if (node === "end") {
+      paths.push(p);
+      return;
+    }
 
-        const neighbours = findNeighbours(vertices, node);
+    const neighbours = findNeighbours(vertices, node);
 
-        for (const neighbour of neighbours) {
-            const canRevisit = neighbour !== 'start' && !neighbour.match(/[a-z]{2}/);
+    for (const neighbour of neighbours) {
+      const canRevisit = neighbour !== "start" && !neighbour.match(/[a-z]{2}/);
 
-            if (canRevisit || !canRevisit && !p.includes(neighbour)) {
-                traverse(neighbour, p);
-            }
-        }
-    };
+      if (canRevisit || !canRevisit && !p.includes(neighbour)) {
+        traverse(neighbour, p);
+      }
+    }
+  };
 
-    traverse('start', ['start']);
+  traverse("start", ["start"]);
 
-    return paths.length;
+  return paths.length;
 };
 
 const findNeighbours = (vertices: string[], node: string) =>
-    vertices.map(v => {
-        const matches = v.match(new RegExp(`${node}\-([a-zA-Z]{2,5})|([a-zA-Z]{2,5})\-${node}`)) || [];
-        return matches[1] || matches[2];
-    })
+  vertices.map((v) => {
+    const matches = v.match(
+      new RegExp(`${node}\-([a-zA-Z]{2,5})|([a-zA-Z]{2,5})\-${node}`),
+    ) || [];
+    return matches[1] || matches[2];
+  })
     .filter(Boolean);
--- 2021practice/day12/path_test.ts
+++ 2021practice/day12/path_test.ts
@@ -4,9 +4,9 @@ import { getNumberOfPermutationsWithUniqueSmallValues } from "./path.ts";
 const input = Deno.readTextFileSync("./2021practice/day12/input.txt");
 
 Deno.test(
-    "getNumberOfPermutationsWithUniqueSmallValues() should return the number of possible paths where nodes with small values are only visited once",
-    () => {
-        const n = getNumberOfPermutationsWithUniqueSmallValues(input);
-        assertEquals(n, 3450);
-    }
+  "getNumberOfPermutationsWithUniqueSmallValues() should return the number of possible paths where nodes with small values are only visited once",
+  () => {
+    const n = getNumberOfPermutationsWithUniqueSmallValues(input);
+    assertEquals(n, 3450);
+  },
 );
