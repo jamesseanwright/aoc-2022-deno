e757a11a0c4c874b59411c4a7a6e13dc87d34014
James Wright <james.wright@matillion.com>
2022-12-01 10:23:59 +0000

    Implements part 2 of day 1

--- day1/elf_calories.ts
+++ day1/elf_calories.ts
@@ -1,16 +1,13 @@
-export const getLargestTotal = (input: string) => {
-    const groups = input.split("\n\n");
+const getSortedTotals = (input: string) =>
+    input.split("\n\n")
+        .map(groups => groups.split("\n"))
+        .map(group => group.reduce((n, x) => n + Number.parseInt(x), 0))
+        .sort((a, b) => b - a);
 
-    let largest = 0;
+export const getLargestTotal = (input: string) =>
+    getSortedTotals(input)[0];
 
-    for (const group of groups) {
-        const items = group.split("\n");
-        const total = items.reduce((n, x) => n + Number.parseInt(x), 0);
-
-        if (total > largest) {
-            largest = total;
-        }
-    }
-
-    return largest;
-};
+export const getTotalForNLargestGroups = (input: string, n: number) =>
+    getSortedTotals(input)
+        .slice(0, n)
+        .reduce((n, x) => n + x, 0);
--- day1/elf_calories_test.ts
+++ day1/elf_calories_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
-import { getLargestTotal } from "./elf_calories.ts";
+import { getLargestTotal, getTotalForNLargestGroups } from "./elf_calories.ts";
 
 const input = Deno.readTextFileSync("./day1/input.txt");
 
@@ -7,4 +7,10 @@ Deno.test("getLargestTotal() should return the total of the largest group", () =
     const largestTotal = getLargestTotal(input);
 
     assertEquals(largestTotal, 71780);
+});
+
+Deno.test("getTotalForNLargestGroups() should return the combined total of the n largest groups", () => {
+    const total = getTotalForNLargestGroups(input, 3);
+
+    assertEquals(total, 212489);
 });
