a5ec2192770cb25554612a4e43be081e6f910a54
James Wright <james.wright@matillion.com>
2022-12-02 10:40:50 +0000

    Implements day 2 part 1

--- day2/rock_paper_scissors.ts
+++ day2/rock_paper_scissors.ts
@@ -1,1 +1,16 @@
-export const calculateScore = (input: string) => 0;
+export const calculateScore = (input: string) => {
+    const pairs = input
+        .split("\n")
+        .map(row => row.split(" "))
+        .filter(([opponent, player]) => opponent && player) // for fileline ending
+        .map(([opponent, player]) => [
+            opponent,
+            String.fromCodePoint(player.codePointAt(0)! - 23)
+        ]);
+
+    return pairs.reduce(
+        (score, [opponent, player]) =>
+            score + Math.min(Math.max((player.codePointAt(0)! - opponent.codePointAt(0)! + 1), -1), 2) * 3 + player.codePointAt(0)! - 'A'.codePointAt(0)! + 1,
+        0
+    );
+};
