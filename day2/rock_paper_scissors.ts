7d8e00be394d6a51fed9ce055d78b701e719d559
James Wright <james.wright@matillion.com>
2022-12-02 10:09:12 +0000

    Adds day 2 test with example and main inputs

---
+++ day2/rock_paper_scissors.ts
@@ -0,0 +1,1 @@
+export const calculateScore = (input: string) => 0;
---
+++ day2/rock_paper_scissors_test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
+import { calculateScore } from "./rock_paper_scissors.ts";
+
+const exampleInput = Deno.readTextFileSync("./day2/example_input.txt");
+const mainInput = Deno.readTextFileSync("./day2/main_input.txt");
+
+Deno.test("calculateScore() should calculate the correct score for the given input", () => {
+  assertEquals(calculateScore(exampleInput), 15);
+});
