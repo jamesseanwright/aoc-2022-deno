35a5e29d3d275bfb13879780563eaa0b7f514e62
James Wright <hi@james.engineering>
2022-12-04 20:30:30 +0000

    Implements day 3 part 2

--- day3/rucksack.ts
+++ day3/rucksack.ts
@@ -2,7 +2,7 @@ export const aggregateCommonRowItemPriorities = (input: string) =>
   input
     .split("\n")
     .filter(Boolean)
-    .map(group => [
+    .map((group) => [
       group.slice(0, group.length / 2),
       group.slice(group.length / 2),
     ])
@@ -11,6 +11,30 @@ export const aggregateCommonRowItemPriorities = (input: string) =>
         (common, c) => right.includes(c) ? c : common,
       );
 
+      const codePoint = commonItem.codePointAt(0) || 0;
+
+      return total + (codePoint >= 97 ? codePoint - 96 : codePoint - 38);
+    }, 0);
+
+export const aggregrateCommonGroupItemPriorities = (
+  input: string,
+  groupSize: number,
+) =>
+  (input
+    .match(new RegExp(`(.*\n){${groupSize}}`, "g")) || [])
+    .map((group) =>
+      group.split("\n").slice(0, groupSize) /* Trim trailing new lines */
+    )
+    .map(([firstGroup, ...restGroups]) => {
+      for (const c of firstGroup) {
+        if (restGroups.every((group) => group.includes(c))) {
+          return c;
+        }
+      }
+
+      return "";
+    })
+    .reduce((total, commonItem) => {
       const codePoint = commonItem.codePointAt(0) || 0;
 
       return total + (codePoint >= 97 ? codePoint - 96 : codePoint - 38);
--- day3/rucksack_test.ts
+++ day3/rucksack_test.ts
@@ -1,10 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
-import { aggregateCommonRowItemPriorities } from "./rucksack.ts";
+
+import {
+  aggregateCommonRowItemPriorities,
+  aggregrateCommonGroupItemPriorities,
+} from "./rucksack.ts";
 
 const exampleInput = Deno.readTextFileSync("./day3/example_input.txt");
 const mainInput = Deno.readTextFileSync("./day3/main_input.txt");
 
-Deno.test("aggregateCommonRowItemPriorities() should compute the sum of the items' priorities that occur in both partitions of a group", () => {
+Deno.test("aggregateCommonRowItemPriorities() should compute the sum of the items' priorities that occur in both partitions of each row", () => {
   assertEquals(aggregateCommonRowItemPriorities(exampleInput), 157);
   assertEquals(aggregateCommonRowItemPriorities(mainInput), 7811);
+});
+
+Deno.test("aggregrateCommonGroupItemPriorities() should compute the sum of the items' priorities that occur once in each row for the configured group size", () => {
+  assertEquals(aggregrateCommonGroupItemPriorities(exampleInput, 3), 70);
+  assertEquals(aggregrateCommonGroupItemPriorities(mainInput, 3), 2639);
 });
