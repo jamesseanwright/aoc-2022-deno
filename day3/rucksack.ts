e7a57579e3fc4b6b4bd1844f80b3d2f33ad29f68
James Wright <hi@james.engineering>
2022-12-04 14:48:37 +0000

    Implements day 3 part 1

--- day3/rucksack.ts
+++ day3/rucksack.ts
@@ -1,1 +1,17 @@
-export const aggregateCommonGroupItemPriorities = (input: string) => 0;
+export const aggregateCommonGroupItemPriorities = (input: string) =>
+  input
+    .split("\n")
+    .filter(Boolean)
+    .map((group) => [
+      group.slice(0, group.length / 2),
+      group.slice(group.length / 2),
+    ])
+    .reduce((total, [left, right]) => {
+      const commonItem = [...left].reduce(
+        (common, c) => right.includes(c) ? c : common,
+      );
+
+      const codePoint = commonItem.codePointAt(0) || 0;
+
+      return total + (codePoint >= 97 ? codePoint - 96 : codePoint - 38);
+    }, 0);
