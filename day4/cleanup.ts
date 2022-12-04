f541a2d5ba40fe90899dedf5493ec7afbfb88373
James Wright <hi@james.engineering>
2022-12-04 21:40:32 +0000

    Implements day 4 part 1

--- day4/cleanup.ts
+++ day4/cleanup.ts
@@ -1,1 +1,13 @@
-export const countPairsWithFullyOverlappingRanges = (input: string) => 0;
+const isWithinRange = (ax: string, an: string, bx: string, bn: string) =>
+  Number.parseInt(ax) >= Number.parseInt(bx) &&
+  Number.parseInt(an) <= Number.parseInt(bn);
+
+export const countPairsWithFullyOverlappingRanges = (input: string) =>
+  [...input.matchAll(/(\d+)-(\d+),(\d+)-(\d+)/g)]
+    .reduce(
+      (total, [, ax, an, bx, bn]) =>
+        isWithinRange(ax, an, bx, bn) || isWithinRange(bx, bn, ax, an)
+          ? total + 1
+          : total,
+      0,
+    );
--- day4/cleanup_test.ts
+++ day4/cleanup_test.ts
@@ -1,13 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 
-import {
-  countPairsWithFullyOverlappingRanges,
-} from "./cleanup.ts";
+import { countPairsWithFullyOverlappingRanges } from "./cleanup.ts";
 
 const exampleInput = Deno.readTextFileSync("./day4/example_input.txt");
 const mainInput = Deno.readTextFileSync("./day4/main_input.txt");
 
 Deno.test("countPairsWithFullyOverlappingRanges() should return the number of pairs where one range fully contains the other", () => {
   assertEquals(countPairsWithFullyOverlappingRanges(exampleInput), 2);
-  assertEquals(countPairsWithFullyOverlappingRanges(mainInput), -1);
+  assertEquals(countPairsWithFullyOverlappingRanges(mainInput), 431);
 });
