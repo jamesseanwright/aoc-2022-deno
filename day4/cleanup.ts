f8b8994ac9c3899b535d5847063c5d164effc84c
James Wright <hi@james.engineering>
2022-12-04 21:59:20 +0000

    Refactors day 4 to centralise sum logic and to take predicates for each case

--- day4/cleanup.ts
+++ day4/cleanup.ts
@@ -1,25 +1,25 @@
-const isWithinRange = (ax: string, an: string, bx: string, bn: string) =>
+type Predicate = (ax: string, an: string, bx: string, bn: string) => boolean;
+
+const areWithinRange = (ax: string, an: string, bx: string, bn: string) =>
   Number.parseInt(ax) >= Number.parseInt(bx) &&
   Number.parseInt(an) <= Number.parseInt(bn);
 
-const overlaps = (ax: string, bx: string, bn: string) =>
+const overlap = (ax: string, _: string, bx: string, bn: string) =>
   Number.parseInt(ax) >= Number.parseInt(bx) &&
   Number.parseInt(ax) <= Number.parseInt(bn);
 
-export const countPairsWithFullyOverlappingRanges = (input: string) =>
+export const countPairsThat = (predicate: Predicate) => (input: string) =>
   [...input.matchAll(/(\d+)-(\d+),(\d+)-(\d+)/g)]
     .reduce(
       (total, [, ax, an, bx, bn]) =>
-        isWithinRange(ax, an, bx, bn) || isWithinRange(bx, bn, ax, an)
+        predicate(ax, an, bx, bn) || predicate(bx, bn, ax, an)
           ? total + 1
           : total,
       0,
     );
 
-export const countPairsWithPartiallyOverlappingRanges = (input: string) =>
-  [...input.matchAll(/(\d+)-(\d+),(\d+)-(\d+)/g)]
-    .reduce(
-      (total, [, ax, an, bx, bn]) =>
-        overlaps(ax, bx, bn) || overlaps(bx, ax, an) ? total + 1 : total,
-      0,
-    );
+export const countPairsWithFullyOverlappingRanges = countPairsThat(
+  areWithinRange,
+);
+
+export const countPairsWithPartiallyOverlappingRanges = countPairsThat(overlap);
