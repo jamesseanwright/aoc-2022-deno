6dffa67df1c428caa223ffabf29a6ce0dee680e1
James Wright <james.wright@matillion.com>
2022-12-06 07:48:44 +0000

    Solves day 6 part 1

--- day6/databuffer.ts
+++ day6/databuffer.ts
@@ -1,1 +1,21 @@
-export const getFirstPacketIndex = (input: string) => 0;
+const PACKET_START_WINDOW = 4;
+
+export const getFirstPacketIndex = (input: string) => {
+  for (let i = 0; i < input.length; i++) {
+    let seen = "";
+
+    for (let j = i; j < i + PACKET_START_WINDOW; j++) {
+      if (seen.includes(input[j])) {
+        break;
+      }
+
+      seen += input[j];
+
+      if (seen.length === PACKET_START_WINDOW) {
+        return j + 1;
+      }
+    }
+  }
+
+  return -1;
+};
--- day6/databuffer_test.ts
+++ day6/databuffer_test.ts
@@ -6,5 +6,5 @@ const mainInput = Deno.readTextFileSync("./day6/main_input.txt");
 
 Deno.test("getFirstPacketIndex() should return the index where the first post-header data packet starts", () => {
   assertEquals(getFirstPacketIndex(exampleInput), 7);
-  assertEquals(getFirstPacketIndex(mainInput), -1);
+  assertEquals(getFirstPacketIndex(mainInput), 1929);
 });
