fb5abac7fed9b29fb8bd9b00592b9ee0e2e185b4
James Wright <hi@james.engineering>
2022-12-09 15:42:55 +0000

    Satiates day 9 part 1 with example input

--- day9/rope.ts
+++ day9/rope.ts
@@ -15,7 +15,7 @@ const getVelocity = (direction: string, steps: number): Point2D => {
   return [0, 0];
 }
 
-const rotate = ([x, y]: Point2D): Point2D => [y * -1, x];
+const rotate = ([x, y]: Point2D): Point2D => [y * -1, x * -1];
 
 const move = (pos: Point2D, steps: Point2D): Point2D =>
   pos.map((a, i) => a + steps[i]) as Point2D;
@@ -38,9 +38,8 @@ export const getTailVisitCount = (input: string) =>
       const distance = getEuclidianDistance(tailPos, newHeadPos);
       let newTailPos = move(tailPos, getVelocity(direction, distance - 1));
 
-      if (areDiagonal(newHeadPos, newTailPos)) {
+      if (Number.parseInt(steps) > 1 && areDiagonal(newHeadPos, newTailPos)) {
         newTailPos = move(newTailPos, rotate(getVelocity(direction, 1)))
-        return [total + distance, newHeadPos, newTailPos]
       }
 
       return [total + distance - 1, newHeadPos, newTailPos]
