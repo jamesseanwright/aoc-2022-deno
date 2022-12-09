11bfc302a7fc51b8ba17807ffc3484990aee0d95
James Wright <hi@james.engineering>
2022-12-09 14:14:29 +0000

    Reads correct input from test

--- day9/rope_test.ts
+++ day9/rope_test.ts
@@ -1,8 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 import { getTailVisitCount } from "./rope.ts";
 
-const exampleInput = Deno.readTextFileSync("./day7/example_input.txt");
-const mainInput = Deno.readTextFileSync("./day7/main_input.txt");
+const exampleInput = Deno.readTextFileSync("./day9/example_input.txt");
+const mainInput = Deno.readTextFileSync("./day9/main_input.txt");
 
 Deno.test("getTailVisitCount() should calculate the number of positions the tail visits from the provided head motions", () => {
   assertEquals(getTailVisitCount(exampleInput), 13);
