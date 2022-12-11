eaeb7942b69d115bd14f54e3249a6204b74ae5a3
James Wright <hi@james.engineering>
2022-12-11 21:26:49 +0000

    Adds all edge nodes to visible total

--- day8/trees.ts
+++ day8/trees.ts
@@ -1,1 +1,23 @@
-export const getVisibleTreesCount = (input: string) => 0;
+export const getVisibleTreesCount = (input: string) => {
+  let visible = 0;
+
+  const grid = input.split("\n")
+    .filter(Boolean) // removes file end line feed
+    .map(row => row.split(""));
+
+  for (let i = 0; i < grid.length; i++) {
+    const row = grid[i];
+
+    for (let j = 0; j < row.length; j++) {
+      const atEdge = i === 0 || i === grid.length - 1 || j === 0 || j === row.length - 1;
+
+      if (atEdge) {
+        visible++;
+      } else {
+
+      }
+    }
+  }
+
+  return visible;
+};
