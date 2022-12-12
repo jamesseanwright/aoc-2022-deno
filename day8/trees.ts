ef3a3f5c9008260392348b15f6f476b413e54973
James Wright <james.wright@matillion.com>
2022-12-12 14:35:39 +0000

    WIP refactors grid traversal

--- day8/trees.ts
+++ day8/trees.ts
@@ -1,5 +1,86 @@
+type TreeScores = [number, number, number, number, number, number];
+
+const getTreeScores = (grid: string[][]) => {
+  const scoreSet: TreeScores[] = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    const row = grid[i];
+
+    for (let j = 0; j < row.length; j++) {
+      let rightScore = 0;
+
+      for (let a = j + 1; a <= row.length; a++) {
+        if (Number.parseInt(row[j]) > Number.parseInt(row[a])) {
+          rightScore++;
+        } else {
+          break;
+        }
+      }
+
+      let leftScore = 0;
+
+      for (let a = j - 1; a >= 0; a--) {
+        if (Number.parseInt(row[j]) > Number.parseInt(row[a])) {
+          leftScore++;
+        } else {
+          break;
+        }
+      }
+
+
+      let bottomScore = 0;
+
+      for (let a = i + 1; a < grid.length; a++) {
+        if (Number.parseInt(grid[i][j]) > Number.parseInt(grid[a][j])) {
+          bottomScore++;
+        } else {
+          break;
+        }
+      }
+
+      let topScore = 0;
+
+      for (let a = i - 1; a >= 0; a--) {
+        if (Number.parseInt(grid[i][j]) > Number.parseInt(grid[a][j])) {
+          topScore++;
+        } else {
+          break;
+        }
+      }
+
+      scoreSet.push([j, i, rightScore, leftScore, bottomScore, topScore]);
+    }
+  }
+
+  return scoreSet;
+}
+
 export const getVisibleTreesCount = (input: string) => {
   const visible = new Set<string>();
+
+  const grid = input.split("\n")
+    .filter(Boolean) // removes file end line feed
+    .map((row) => row.split(""));
+
+  const rowCount = grid.length;
+  const colCount = grid[0].length;
+
+  getTreeScores(grid).forEach(([x, y, right, left, bottom, top]) => {
+    if (
+      x + right === rowCount - 1 ||
+      x - left === 0 ||
+      y + bottom === colCount - 1 ||
+      y - top === 0
+    ) {
+      visible.add(`${x}-${y}`);
+    }
+  });
+
+  return visible.size;
+};
+
+export const getHighestScenicScore = (input: string) => {
+  const scores = new Set<number>();
 
   const grid = input.split("\n")
     .filter(Boolean) // removes file end line feed
@@ -9,61 +90,50 @@ export const getVisibleTreesCount = (input: string) => {
     const row = grid[i];
 
     for (let j = 0; j < row.length; j++) {
-      let isVisibleFromRight = true;
+      let rightScore = 0;
 
       for (let a = j + 1; a < row.length; a++) {
+        rightScore++;
+
         if (Number.parseInt(row[j]) <= Number.parseInt(row[a])) {
-          isVisibleFromRight = false;
           break;
         }
       }
 
-      if (isVisibleFromRight) {
-        visible.add(`${i}-${j}`);
-      }
-
-      let isVisibleFromLeft = true;
+      let leftScore = 0;
 
       for (let a = j - 1; a >= 0; a--) {
+        leftScore++;
+
         if (Number.parseInt(row[j]) <= Number.parseInt(row[a])) {
-          isVisibleFromLeft = false;
           break;
         }
       }
 
-      if (isVisibleFromLeft) {
-        visible.add(`${i}-${j}`);
-      }
 
-      let isVisibleFromBottom = true;
+      let bottomScore = 0;
 
       for (let a = i + 1; a < grid.length; a++) {
+        bottomScore++;
+
         if (Number.parseInt(grid[i][j]) <= Number.parseInt(grid[a][j])) {
-          isVisibleFromBottom = false;
           break;
         }
       }
 
-      if (isVisibleFromBottom) {
-        visible.add(`${i}-${j}`);
-      }
-
-      let isVisibleFromTop = true;
+      let topScore = 0;
 
       for (let a = i - 1; a >= 0; a--) {
+        topScore++;
+
         if (Number.parseInt(grid[i][j]) <= Number.parseInt(grid[a][j])) {
-          isVisibleFromTop = false;
           break;
         }
       }
 
-      if (isVisibleFromTop) {
-        visible.add(`${i}-${j}`);
-      }
+      scores.add(rightScore * leftScore * bottomScore * topScore);
     }
   }
 
-  return visible.size;
+  return [...scores.values()].toSorted((a, b) => b - a)[0];
 };
-
-export const getHighestScenicScore = (input: string) => 0;
