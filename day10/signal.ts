ee5d1668fcce17fcb9553e29f17897287a3407a3
James Wright <james.wright@matillion.com>
2022-12-12 22:39:43 +0000

    Adds support for callback return values and refactors exported functions to follow functional paradigm

--- day10/signal.ts
+++ day10/signal.ts
@@ -2,11 +2,13 @@ const SIGNAL_START_CYCLE = 20;
 const SIGNAL_CYCLE_INTERVAL = 40;
 const CRT_SIZE = 240;
 
-const executeInstructions = (
+const executeInstructions = <T>(
   input: string,
   maxCycles: number,
-  onCycle: (cycle: number, x: number) => void,
-) => {
+  onCycle: (cycle: number, x: number) => T,
+): T[] => {
+  const results: T[] = [];
+
   const instructions = input
     .split("\n")
     .filter(Boolean) // !file line ending
@@ -16,7 +18,7 @@ const executeInstructions = (
   const stack: [string, string][] = [];
 
   for (let cycle = 1; cycle <= maxCycles; cycle++) {
-    onCycle(cycle, x);
+    results.push(onCycle(cycle, x));
 
     if (stack.length > 0) {
       const [instruction, arg] = stack.pop()!;
@@ -32,34 +34,29 @@ const executeInstructions = (
       }
     }
   }
+
+  return results;
 };
 
-export const getSignalStrength = (input: string, maxCycles: number) => {
-  let signalStrength = 0;
+export const getSignalStrength = (input: string, maxCycles: number) =>
+  executeInstructions(
+    input,
+    maxCycles,
+    (cycle, x) =>
+      (cycle - SIGNAL_START_CYCLE) % SIGNAL_CYCLE_INTERVAL === 0
+        ? cycle * x
+        : 0,
+  ).reduce((total, x) => total + x, 0);
 
-  executeInstructions(input, maxCycles, (cycle, x) => {
-    if ((cycle - SIGNAL_START_CYCLE) % SIGNAL_CYCLE_INTERVAL === 0) {
-      signalStrength += cycle * x;
-    }
-  });
-
-  return signalStrength;
-};
-
-export const run = (input: string) => {
-  const screen: boolean[] = [];
-
+export const run = (input: string) =>
   executeInstructions(input, CRT_SIZE, (cycle, x) => {
     const pixel = cycle - 1;
 
-    screen[pixel] = (pixel % SIGNAL_CYCLE_INTERVAL) >= x - 1 &&
+    return (pixel % SIGNAL_CYCLE_INTERVAL) >= x - 1 &&
       (pixel % SIGNAL_CYCLE_INTERVAL) <= x + 1;
-  });
-
-  return screen.reduce(
+  }).reduce(
     (out, pixel, i) =>
       out + (pixel ? "#" : ".") +
       ((i + 1) % SIGNAL_CYCLE_INTERVAL === 0 ? "\n" : ""),
     "",
   );
-};
