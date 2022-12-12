c179b5796b0425bc213f86e8d17cd0cd207a6c68
James Wright <james.wright@matillion.com>
2022-12-12 12:32:52 +0000

    Updates day 8 part 2 test assertions

--- day8/trees_test.ts
+++ day8/trees_test.ts
@@ -6,10 +6,10 @@ const mainInput = Deno.readTextFileSync("./day8/main_input.txt");
 
 Deno.test("getVisibleTreesCount() should return the number of trees that are visible from an edge of the grid", () => {
   assertEquals(getVisibleTreesCount(exampleInput), 21);
-  assertEquals(getVisibleTreesCount(mainInput), -1);
+  assertEquals(getVisibleTreesCount(mainInput), 1684);
 });
 
 Deno.test("getHighestScenicScore() should return the highest possible scenic score for any tree in the grid", () => {
   assertEquals(getHighestScenicScore(exampleInput), 8);
-  assertEquals(getHighestScenicScore(mainInput), -1);
+  assertEquals(getHighestScenicScore(mainInput), 486540);
 });
