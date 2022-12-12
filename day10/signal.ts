acfb811cb0bc5bf43e270dc325fe7ce8e078354e
James Wright <james.wright@matillion.com>
2022-12-12 22:04:22 +0000

    First line rendering for day 10 part 2

--- day10/signal.ts
+++ day10/signal.ts
@@ -46,6 +46,9 @@ export const run = (input: string) => {
   const screen = Array.from<boolean>({ length: CRT_SIZE }).fill(false);
 
   for (let cycle = 1; cycle <= CRT_SIZE; cycle++) {
+    const pixel = cycle - 1;
+    screen[pixel] = pixel >= x-1 && pixel <= x+1;
+
     if (stack.length > 0) {
       const [instruction, arg] = stack.pop()!;
 
@@ -59,8 +62,6 @@ export const run = (input: string) => {
         stack.push([instruction, arg]);
       }
     }
-
-    screen[cycle - 1] = cycle >= x-1 && cycle <= x+1;
   }
 
   return screen.reduce(
