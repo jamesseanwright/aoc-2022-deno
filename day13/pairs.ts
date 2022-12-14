76a231445d5dc7e53d56bd249925024cd73fecc3
James Wright <hi@james.engineering>
2022-12-14 13:44:14 +0000

    Lifts compare() out of main func and converts reduce to for to bail early

--- day13/pairs.ts
+++ day13/pairs.ts
@@ -1,28 +1,30 @@
-const isList = (x: string) => x[0] === "[";
+const isList = (x?: string) => x?.[0] === "[";
 
 // Note that this only parses the
 // top-level into an array; any child
 // lists are then parsed just-in-time.
 const parseList = (x: string) => isList(x) ? x.slice(1, -1).split(",") : [x];
 
+const compare = (a: string[], b: string[]): boolean => {
+  for (let i = 0; i < a.length; i++) {
+    const left = a[i];
+    const right = b[i];
+
+    if (isList(left) || isList(right)) {
+      return compare(parseList(left), parseList(right));
+    }
+
+    if (Number.parseInt(left) < Number.parseInt(right)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 export const getPacketPairIntegritySum = (input: string) => {
   const pairs = [...input.matchAll(/(\[.*\])\n(\[.*\])\n\n/g)]
     .map(([, ...pair]) => pair);
-
-  const compare = (a: string[], b: string[], areEqual = false): boolean =>
-    a.reduce((equal, left, i) => {
-      const right = b[i];
-
-      if (equal || !right) {
-        return equal;
-      }
-
-      if (isList(left) || isList(right)) {
-        return compare(parseList(left), parseList(right), areEqual);
-      }
-
-      return Number.parseInt(left) < Number.parseInt(right);
-    }, areEqual);
 
   return pairs.reduce(
     (total, [a, b], i) =>
