f3429acdc33f97972e2210874473a52ed2b39c71
James Wright <hi@james.engineering>
2022-12-14 12:53:15 +0000

    Formats code

--- day13/pairs.ts
+++ day13/pairs.ts
@@ -1,11 +1,9 @@
-const isList = (x: string) => x[0] === '[';
+const isList = (x: string) => x[0] === "[";
 
 // Note that this only parses the
 // top-level into an array; any child
 // lists are then parsed just-in-time.
-const parseList = (x: string) => isList(x)
-  ? x.slice(1, -1).split(",")
-  : [x];
+const parseList = (x: string) => isList(x) ? x.slice(1, -1).split(",") : [x];
 
 export const getPacketPairIntegritySum = (input: string) => {
   const pairs = [...input.matchAll(/(\[.*\])\n(\[.*\])\n\n/g)]
@@ -26,5 +24,9 @@ export const getPacketPairIntegritySum = (input: string) => {
       return Number.parseInt(left) < Number.parseInt(right);
     }, areEqual);
 
-  return pairs.reduce((total, [a, b], i) => compare(parseList(a), parseList(b)) ? total + i + 1 : total, 0)
-}
+  return pairs.reduce(
+    (total, [a, b], i) =>
+      compare(parseList(a), parseList(b)) ? total + i + 1 : total,
+    0,
+  );
+};
