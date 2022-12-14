33ade35ff490eec1a8913ec21e6cd94da2172252
James Wright <hi@james.engineering>
2022-12-14 12:49:44 +0000

    Lazily/JIT parses lists into JS arrays

--- day13/pairs.ts
+++ day13/pairs.ts
@@ -1,27 +1,35 @@
-const isList = (x: string) => x === '[';
+// Note that this only parses the
+// top-level into an array; any child
+// lists are then parsed just-in-time.
+const parseList = (list: string) => list.slice(1, -1).split(",");
+const isList = (x: string) => x[0] === '[';
 
 export const getPacketPairIntegritySum = (input: string) => {
   const pairs = [...input.matchAll(/(\[.*\])\n(\[.*\])\n\n/g)]
     .map(([, ...pair]) => pair);
 
-  const compare = (a: string, b: string, areEqual = true): boolean =>
-    [...a].reduce((equal, left, i) => {
+  const compare = (a: string[], b: string[], areEqual = false): boolean =>
+    a.reduce((equal, left, i) => {
       const right = b[i];
 
-      if (!right) {
-        return areEqual;
+      if (equal || !right) {
+        return equal;
+      }
+
+      if (isList(left) && isList(right)) {
+        return compare(parseList(left), parseList(right), areEqual);
       }
 
       if (!isList(left) && isList(right)) {
-        return compare(left, b.slice(1));
+        return compare([left], parseList(right), areEqual);
       }
 
       if (isList(left) && !isList(right)) {
-        return compare(a.slice(1), right);
+        return compare(parseList(left), [right], areEqual);
       }
 
-      return equal && Number.parseInt(left) < Number.parseInt(right);
+      return Number.parseInt(left) < Number.parseInt(right);
     }, areEqual);
 
-  return pairs.reduce((total, [a, b]) => compare(a, b) ? total + 1 : total, 0)
+  return pairs.reduce((total, [a, b]) => compare(parseList(a), parseList(b)) ? total + 1 : total, 0)
 }
