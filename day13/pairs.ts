d45704beb8bad67f22f854e7e056085138199522
James Wright <hi@james.engineering>
2022-12-14 12:29:24 +0000

    WIP sketch of day 13 part 1

--- day13/pairs.ts
+++ day13/pairs.ts
@@ -1,1 +1,27 @@
-export const getPacketPairIntegritySum = (input: string) => 0;
+const isList = (x: string) => x === '[';
+
+export const getPacketPairIntegritySum = (input: string) => {
+  const pairs = [...input.matchAll(/(\[.*\])\n(\[.*\])\n\n/g)]
+    .map(([, ...pair]) => pair);
+
+  const compare = (a: string, b: string, areEqual = true): boolean =>
+    [...a].reduce((equal, left, i) => {
+      const right = b[i];
+
+      if (!right) {
+        return areEqual;
+      }
+
+      if (!isList(left) && isList(right)) {
+        return compare(left, right.slice(1));
+      }
+
+      if (isList(left) && !isList(right)) {
+        return compare(left.slice(1), right);
+      }
+
+      return equal && Number.parseInt(left) < Number.parseInt(right);
+    }, areEqual);
+
+  return pairs.reduce((total, [a, b]) => compare(a, b) ? total + 1 : total, 0)
+}
