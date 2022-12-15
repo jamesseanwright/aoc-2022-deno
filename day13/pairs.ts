7083f9414eb654f7ad9b3b58b035fbf0be6c36c8
James Wright <james.wright@matillion.com>
2022-12-15 09:09:47 +0000

    WIP refactor to parse lists ahead of time

--- day13/pairs.ts
+++ day13/pairs.ts
@@ -1,18 +1,40 @@
-const isList = (x?: string) => x?.[0] === "[";
+type Item = string | string[];
 
-// Note that this only parses the
-// top-level into an array; any child
-// lists are then parsed just-in-time.
-const parseList = (x: string) => isList(x) ? x.slice(1, -1).split(",") : [x];
+const isList = (x: string) => x === '[';
 
-const compare = (a: string[], b: string[]): boolean => {
-  console.log('*****', a, b);
+const parseList = (list: string): Item[] => {
+  const arr: Item[] = [];
+
+  const walk = (x: string, i = 1, out = arr) => {
+    if (i === x.length - 1) {
+      return;
+    }
+
+    if (isList(x)) {
+      const children: string[] = [];
+      out.push(children);
+      walk(x, i + 1, children);
+    } else {
+      walk(x, i + 1, out);
+    }
+  };
+
+  walk(list);
+
+  return arr;
+};
+
+const coerce = (x: Item) => Array.isArray(x)
+  ? x
+  : [x];
+
+const compare = (a: Item[], b: Item[]): boolean => {
   for (let i = 0; i < b.length; i++) {
     const right = b[i];
     const left = a[i];
 
-    if (isList(left) || isList(right)) {
-      return compare(parseList(left), parseList(right));
+    if (Array.isArray(left) || Array.isArray(right)) {
+      return compare(coerce(left), coerce(right));
     }
 
     if (right && !left || Number.parseInt(left) < Number.parseInt(right)) {
@@ -25,11 +47,12 @@ const compare = (a: string[], b: string[]): boolean => {
 
 export const getPacketPairIntegritySum = (input: string) => {
   const pairs = [...input.matchAll(/(\[.*\])\n(\[.*\])\n\n/g)]
-    .map(([, ...pair]) => pair);
+    .map(([, ...pair]) => pair)
+    .map(([left, right]) => [parseList(left), parseList(right)]);
 
   return pairs.reduce(
     (total, [a, b], i) =>
-      compare(parseList(a), parseList(b)) ? total + i + 1 : total,
+      compare(a, b) ? total + i + 1 : total,
     0,
   );
 };
