69698e5897f80d87004ca28aa01bacd9ff16b855
James Wright <hi@james.engineering>
2022-12-18 16:50:53 +0000

    Adds boilerplate for day 12

---
+++ day12/hill.ts
@@ -0,0 +1,1 @@
+export const getShortestPathStepCount = (input: string) => 0;
---
+++ day12/hill_test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
+import { getShortestPathStepCount } from "./hill.ts";
+
+const exampleInput = Deno.readTextFileSync("./day12/example_input.txt");
+const mainInput = Deno.readTextFileSync("./day12/main_input.txt");
+
+Deno.test("getShortestPathStepCount() should return the number of steps for the shortest path from S to E", () => {
+  assertEquals(getShortestPathStepCount(exampleInput), 31);
+  assertEquals(getShortestPathStepCount(mainInput), -1);
+});
