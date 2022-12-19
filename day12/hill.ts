462238f5f1975d1832722e5e7c79040f63d32cd5
James Wright <hi@james.engineering>
2022-12-19 12:57:25 +0000

    Refactors day 12 as there's no value to building graph

--- day12/hill.ts
+++ day12/hill.ts
@@ -1,87 +1,28 @@
 const SOURCE_NODE_VALUE = "S";
 const TARGET_NODE_VALUE = "E";
 
-interface Node<T> {
-  north?: Node<T>;
-  south?: Node<T>;
-  east?: Node<T>;
-  west?: Node<T>;
-  value?: T;
-}
-
-const createNode = <T>(
-  x: number,
-  y: number,
-  nodes: Map<string, Node<T>>,
-): Node<T> => {
-  if (nodes.has(`${x}-${y}`)) {
-    return nodes.get(`${x}-${y}`)!;
-  }
-
-  const node = {};
-
-  nodes.set(`${x}-${y}`, node);
-
-  return node;
-};
-
-const buildGraph = (rows: string[]): Node<string> => {
-  const nodes = new Map<string, Node<string>>();
-  let sourcePos = [0, 0];
-
-  for (let y = 0; y < rows.length; y++) {
-    for (let x = 0; x < rows[y].length; x++) {
-      const node = createNode(x, y, nodes);
-      const value = rows[y][x];
-
-      node.value = value;
-
-      if (node.value === SOURCE_NODE_VALUE) {
-        sourcePos = [x, y];
-      }
-
-      if (x > 0) {
-        node.west = createNode(x - 1, y, nodes);
-      }
-
-      if (x < rows[y].length - 1) {
-        node.east = createNode(x + 1, y, nodes);
-      }
-
-      if (y > 0) {
-        node.north = createNode(x, y - 1, nodes);
-      }
-
-      if (y < rows.length - 1) {
-        node.south = createNode(x, y + 1, nodes);
-      }
-    }
-  }
-
-  return nodes.get(sourcePos.join("-")) || {};
-};
-
 const getCodePoint = (x?: string) => (x?.codePointAt(0) || 0);
 
 export const getShortestPathStepCount = (input: string) => {
   const rows = input.split("\n").filter(Boolean); // LF
-  const source = buildGraph(rows);
+  let start: [number, number] = [-1, -1];
 
-  const traverse = (node: Node<string>, steps = 0) => {
-    if (node.value === TARGET_NODE_VALUE) {
+  for (let y = 0; y < rows.length; y++) {
+    for (let x = 0; x < rows[y].length; x++) {
+      if (rows[y][x] === SOURCE_NODE_VALUE) {
+        start = [x, y];
+      }
+    }
+  }
+
+  const getPaths = ([x, y]: [number, number], steps = 0) => {
+    if (rows[y][x] === TARGET_NODE_VALUE) {
       return steps;
     }
 
-    [node.north, node.south, node.east, node.west].forEach((neighbour) => {
-      if (
-        neighbour &&
-        Math.abs(getCodePoint(neighbour.value) - getCodePoint(node.value)) <= 1
-      ) {
-        traverse(neighbour, steps + 1);
-      }
-    });
+    // TODO: walk
   };
 
-  console.log("****", traverse(source!));
-  return traverse(source!);
+  console.log("****", getPaths(start));
+  return 0;
 };
