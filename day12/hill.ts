cf9ae868e0c33c47e0034622c7d2a64e1a3fd735
James Wright <hi@james.engineering>
2022-12-19 13:44:33 +0000

    WIP path traversal

--- day12/hill.ts
+++ day12/hill.ts
@@ -1,11 +1,13 @@
 const SOURCE_NODE_VALUE = "S";
 const TARGET_NODE_VALUE = "E";
 
+type Cell = [number, number];
+
 const getCodePoint = (x?: string) => (x?.codePointAt(0) || 0);
 
 export const getShortestPathStepCount = (input: string) => {
   const rows = input.split("\n").filter(Boolean); // LF
-  let start: [number, number] = [-1, -1];
+  let start: Cell = [-1, -1];
 
   for (let y = 0; y < rows.length; y++) {
     for (let x = 0; x < rows[y].length; x++) {
@@ -15,14 +17,32 @@ export const getShortestPathStepCount = (input: string) => {
     }
   }
 
-  const getPaths = ([x, y]: [number, number], steps = 0) => {
+  const getPaths = ([x, y]: Cell, currentPath: Cell[], paths: Cell[][]) => {
+    currentPath.push([x, y]);
+
     if (rows[y][x] === TARGET_NODE_VALUE) {
-      return steps;
+      return;
     }
 
-    // TODO: walk
+    ([[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]] as Cell[])
+      .forEach(([nx, ny]) => {
+        const canVisit = rows[ny]?.[nx] &&
+          !currentPath.some(([vx, vy]) => nx === vx && vy === vy) &&
+          (rows[y][x] === SOURCE_NODE_VALUE ||
+            Math.abs(getCodePoint(rows[ny][nx]) - getCodePoint(rows[y][x])) <=
+              1);
+
+        if (canVisit) {
+          getPaths([nx, ny], [...currentPath], paths);
+        } else {
+          paths.push(currentPath);
+        }
+      });
   };
 
-  console.log("****", getPaths(start));
-  return 0;
+  const paths: Cell[][] = [];
+
+  getPaths(start, [], paths);
+
+  return paths.toSorted((a, b) => a.length - b.length)[0].length;
 };
