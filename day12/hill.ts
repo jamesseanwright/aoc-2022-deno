1976e3f679831b69cd3318b55396ca4442ae2986
James Wright <hi@james.engineering>
2022-12-19 12:01:51 +0000

    Returns source and target nodes from buildGraph()

--- day12/hill.ts
+++ day12/hill.ts
@@ -1,3 +1,6 @@
+const SOURCE_NODE_VALUE = "S";
+const TARGET_NODE_VALUE = "E";
+
 interface Node<T> {
   north?: Node<T>;
   south?: Node<T>;
@@ -18,15 +21,25 @@ const createNode = <T>(x: number, y: number, nodes: Map<string, Node<T>>): Node<
   return node;
 };
 
-const buildGraph = (rows: string[]): Node<string> => {
+const buildGraph = (rows: string[]): [Node<string>?, Node<string>?] => {
   const nodes = new Map<string, Node<string>>();
+  let sourcePos = [0, 0];
+  let targetPos = [0, 0];
 
   for (let y = 0; y < rows.length; y++) {
     for (let x = 0; x < rows[y].length; x++) {
       const node = createNode(x, y, nodes);
       const value = rows[y][x];
 
-      node.value = value
+      node.value = value;
+
+      if (node.value === SOURCE_NODE_VALUE) {
+        sourcePos = [x, y];
+      }
+
+      if (node.value === TARGET_NODE_VALUE) {
+        targetPos = [x, y];
+      }
 
       if (x > 0) {
         node.west = createNode(x - 1, y, nodes);
@@ -46,13 +59,12 @@ const buildGraph = (rows: string[]): Node<string> => {
     }
   }
 
-  return nodes.get('0-0') || {};
+  return [nodes.get(sourcePos.join("-")), nodes.get(targetPos.join("-"))];
 };
 
 export const getShortestPathStepCount = (input: string) => {
   const rows = input.split("\n").filter(Boolean); // LF
-  const graph = buildGraph(rows);
-  // const start = findStart(graph);
+  const [source, target] = buildGraph(rows);
 
   return 0;
 };
